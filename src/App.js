a80ccd8cef85c2ebca07adee9ccd5e0a1f544a8c
gayoon choi <doll2299@naver.com>
2020-07-09 18:46:22 +0900

    APP Sass 연동 및 state 설계
    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React,{ Component } from 'react';
-import style  from './App.scss';
+import React, { Component } from 'react';
+import style from './App.scss';
 import className from 'classnames';
+import * as api from '../src/lib/api';
+import NewsContainer from '../src/containers/NewsContainer'
 
 const ex = className.bind(style);
 
 class App extends Component {
-  render(){
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {
+      selecteItems : [] 
+    };
+  }
+
+  componentDidMount() {
+    api.getApi()
+    .then(res =>this.setState({
+      selecteItems : res.data.items
+    }))
+  }
+
+  
+  render() {
+    return (
       <div className={ex('content')}>
-        ㅎㅇㅎㅇ
+          <NewsContainer selecteItems = {this.state.selecteItems}/>
       </div>
     )
   }
